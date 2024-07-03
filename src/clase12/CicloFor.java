203e74b1ba7b61b459707f98c1e14a58a706a906
ctapiadev <ctapiadev@gmail.com>
2024-07-02 20:24:57 -0400

    clase 12 ciclos

---
+++ src/clase12/CicloFor.java
@@ -0,0 +1,13 @@
+package clase12;
+
+public class CicloFor {
+    
+    public static void main(String[] args) {
+    
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        
+    }
+    
+}
---
+++ src/clase12/CicloWhile.java
@@ -0,0 +1,17 @@
+package clase12;
+
+public class CicloWhile {
+    
+    public static void main(String[] args) {
+        
+        int a = 0;
+        int b = 10;
+        
+        while (a < b) {
+            System.out.println(a);
+            a++;
+        }
+        
+    }
+    
+}
