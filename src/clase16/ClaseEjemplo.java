25a0ac2cd9b1fb20f9d08d5a8bdb8e33783466bb
ctapiadev <ctapiadev@gmail.com>
2024-07-09 20:50:03 -0400

    clase 16 estandares

---
+++ src/clase16/ClaseEjemplo.java
@@ -0,0 +1,9 @@
+package clase16;
+
+public class ClaseEjemplo {
+    
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
---
+++ src/clase16/Estandares.java
@@ -0,0 +1,55 @@
+package clase16;
+
+import java.util.*;
+
+public class Estandares {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+
+        /*
+            System.out.println("Lorem ipsum dolor sit amet,"
+                    + "consectetur adipiscing elit. Cras egestas ultricies justo,"
+                    + "at faucibus neque ullamcorper vel."
+                    + "Sed aliquet dui id consectetur vulputate."
+                    + "Cras ultricies ullamcorper libero,"
+                    + "id ultrices tellus pharetra id."
+                    + "Integer sodales sem bibendum quam bibendum condimentum."
+                    + "Donec arcu orci, convallis ornare imperdiet eu,"
+                    + "lobortis vitae orci. Duis sit amet dapibus ligula,"
+                    + "eu tincidunt ex. Donec lacinia ligula eu justo tincidunt fermentum."
+                    + "Nunc efficitur mollis dolor,"
+                    + "non pulvinar quam elementum vitae. Nam vel fermentum arcu."
+                    + "In hac habitasse platea dictumst. Quisque vel pellentesque libero.");
+        */
+        
+        // Comentario simple
+        /* Comentario simple de otra forma */
+        /***** TITULO *****/
+        
+        int contador = 0; // Contador inicializado
+        contador++; // Aumento de contador
+        
+        int x = 0;
+//        String cadena = input.next();
+
+//        while (true) {
+//            
+//        }
+
+        int suma = 5+5;
+        
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 10; j++) {
+//                
+//            }
+//        }
+
+        String saludoCompleto = "Hola Mundo";
+        System.out.println(saludoCompleto);
+        
+        final int LONGITUD_MAXIMA = 20; // Variable constante
+    }
+}
